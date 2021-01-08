4c4c98c79ffdd4c6b55bbe9cff522b69bc70233e
Prakriti Dhir <p2dhir@uwaterloo.ca>
2021-01-07 20:50:00 -0500

    Tutorial 2

---
+++ tut2.js
@@ -0,0 +1,12 @@
+let firstVar;
+firstVar = 'Hello World';
+
+let firstVar = 'Hello World';
+
+let a = 'a', b = 'b', c = 'c';
+
+let dupVal;
+
+dupVal = firstVar; // dupVal = 'Hello World'
+
+firstVar = 'Modified Val';
